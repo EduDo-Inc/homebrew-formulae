e506eafbdd8956e1a5258402e0cfddc67b6ad3c2
maximkrouk <id.maximkrouk@gmail.com>
2021-05-09 23:15:25 +0300

    clean: brew warnings

--- Formulae/spmgen@latest.rb
+++ Formulae/spmgen@latest.rb
@@ -1,19 +1,19 @@
 # frozen_string_literal: true
 
 class SPMGen < Formula
-    desc "Resources boilerplate code generator for Swift"
-    homepage "https://github.com/edudo-inc/spmgen"
-    url "https://github.com/edudo-inc/spmgen.git", branch: "main"
-    version "latest"
+  desc "Resources boilerplate code generator for Swift"
+  homepage "https://github.com/edudo-inc/spmgen"
+  url "https://github.com/edudo-inc/spmgen.git", branch: "main"
+  version "latest"
 
-    depends_on xcode: ["12.5", :build]
-    
-    def install
-      system "swift", "build", "-c", "release", "--disable-sandbox", "--build-path", "'.build'"
-      bin.install ".build/release/swift-format"
-    end
+  depends_on xcode: ["12.5", :build]
 
-    test do
-      system "#{bin}/swift-format", "--version"
-    end
+  def install
+    system "swift", "build", "-c", "release", "--disable-sandbox", "--build-path", "'.build'"
+    bin.install ".build/release/swift-format"
+  end
+
+  test do
+    system "#{bin}/swift-format", "--version"
+  end
 end
