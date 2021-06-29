9b9349043e61654c231f80e64e2e6d01917e7566
maximkrouk <id.maximkrouk@gmail.com>
2021-06-29 16:53:52 +0300

    fix: revert install using bash

--- Formulae/swift-format.rb
+++ Formulae/swift-format.rb
@@ -4,12 +4,14 @@ class SwiftFormat < Formula
   desc "Code formatter, based on SourceKit-LSP"
   homepage "https://github.com/edudo-inc/swift-format"
   url "https://github.com/edudo-inc/swift-format.git", branch: "edudo"
-  version "edudo"
+  version "latest"
 
   depends_on xcode: ["12.5", :build]
 
   def install
-    system "`make install_clean`"
+    system "swift", "package", "clean"
+    system "swift", "build", "-c", "release", "--disable-sandbox", "-build-path", "'.build'"
+    bin.install ".build/release/swift-format"
   end
 
   test do
