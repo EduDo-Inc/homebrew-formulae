3d16d8389d3d4ba69f86b6af6b919265a2b9e069
maximkrouk <id.maximkrouk@gmail.com>
2021-06-29 16:29:01 +0300

    feat: Use make for installation

--- Formulae/swift-format.rb
+++ Formulae/swift-format.rb
@@ -4,12 +4,12 @@ class SwiftFormat < Formula
   desc "Code formatter, based on SourceKit-LSP"
   homepage "https://github.com/edudo-inc/swift-format"
   url "https://github.com/edudo-inc/swift-format.git", branch: "edudo"
-  version "latest"
+  version "edudo"
 
   depends_on xcode: ["12.5", :build]
 
   def install
-    system "make", "install_clean"
+    system "`make install_clean`"
   end
 
   test do
